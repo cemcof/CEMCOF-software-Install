40aa26ea21996fc70d287b53b4f87739903e0cbe
Ivo Durnik <ivo.durnik@ceitec.muni.cz>
2022-09-14 08:06:49 +0200

    relion

---
+++ em/relion/4.0_220321/m64-deb10/para/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export DIR="/cemcofsw/em/relion/4.0_220321/m64-deb10/para"
+
+rm -rf $DIR/scripts $DIR/data
+
+(
+    cd relion
+
+    rm -rf build
+    mkdir build
+    (
+        cd build
+
+        module add cuda/11.6.1
+        module add openmpi/4.1.2/m64-deb10
+
+        cmake -DCMAKE_INSTALL_PREFIX=$DIR -DCUDA=ON -DCUDA_ARCH="--gpu-architecture=compute_50 --gpu-code=compute_50,sm_75,sm_86" .. 2>&1 | tee configure.log
+
+        make -j4 2>&1 | tee make.log
+
+        make install 2>&1 | tee install.log
+
+        cp *.log ../..
+    )
+    cp -r scripts data $DIR
+)
+
