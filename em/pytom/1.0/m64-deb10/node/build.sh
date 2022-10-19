caf4b87b3e1c93b498038ea4455df915a069e0e6
Ivo Durnik <ivo.durnik@ceitec.muni.cz>
2022-10-19 17:28:29 +0200

    pytom/1.0 - update, WIP

--- em/pytom/1.0/m64-deb10/node/build.sh
+++ em/pytom/1.0/m64-deb10/node/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DIR=/cemcofsw/em/pytom/1.0/x86_64/node
+DIR=/cemcofsw/em/pytom/1.0/m64-deb10/node
 
 # clean
 rm -rf $DIR/*
@@ -21,8 +21,41 @@ rm -rf $DIR/*
     python3.8 setup.py install --prefix $DIR/conda/envs/pytom_env
 ) 2>&1 | tee pytom.log
 
-mkdir -p $DIR/bin
-cp -av pytom.tmp $DIR/bin/pytom
-cp -av ipytom.tmp $DIR/bin/ipytom
-cp -av pytomGUI.tmp $DIR/bin/pytomGUI
+# gnureadline - solves arrow key problems in python shell
+(
+    eval "$($DIR/conda/bin/conda shell.bash hook)"
+
+    conda activate pytom_env
+
+    pip install gnureadline
+) 2>&1 | tee gnureadline.log
+
+# Wrappers for pytom
+(
+    mkdir -p $DIR/bin
+    cp -av pytom.tmp $DIR/bin/pytom
+    cp -av ipytom.tmp $DIR/bin/ipytom
+    cp -av pytomGUI.tmp $DIR/bin/pytomGUI
+
+    for PY in $DIR/conda/envs/pytom_env/bin/*.py; do
+        SHEBANG=`head -1 -q $PY`
+        if [[ $SHEBANG =~ env[[:blank:]]+pytom ]]; then
+            BASE=`basename $PY`
+
+            echo "#!/bin/bash" > $DIR/bin/$BASE
+            echo "" >> $DIR/bin/$BASE
+            echo "DIR=$DIR" >> $DIR/bin/$BASE
+            echo "eval \"\$(\$DIR/conda/bin/conda shell.bash hook)\"" >> $DIR/bin/$BASE
+            echo "conda activate pytom_env" >> $DIR/bin/$BASE
+            echo "" >> $DIR/bin/$BASE
+            echo "$BASE \"\$@\"" >> $DIR/bin/$BASE
+
+            chmod a+x $DIR/bin/$BASE
+
+            echo "$PY -> $DIR/bin/$BASE"
+        else
+            echo "Skipped: $PY"
+        fi
+    done
+) 2>&1 | tee pytom_wrappers.log
 
